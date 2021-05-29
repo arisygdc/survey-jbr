53f3a9e2d0d9169b98f763abe050a64b16749110
arisygdc <arisycso@gmail.com>
2021-05-29 15:56:24 +0700

    Revert "Text not pop up"
    This reverts commit eaa275c2dca27cf261de70570a4b1023644368c5.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,11 +51,6 @@
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
-                                @role('admin')
-                                
-                                @else
-
-                                @endrole
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
