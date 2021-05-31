b46a830ec831cebfebc3ffe90230cafa2883414c
arisygdc <arisycso@gmail.com>
2021-05-31 23:17:27 +0700

    Create new link per role

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,6 +47,18 @@
                             @endif
 
                         @else
+                            @role('admin')
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('store.user') }}">{{ __('Register') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('get.users') }}">{{ __('Users') }}</a>
+                            </li>
+                            @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('survey') }}">{{ __('Survey') }}</a>
+                            </li>
+                            @endrole
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
