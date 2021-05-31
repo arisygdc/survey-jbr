53ed66b511c46218720b060932ae2cc09cc4f83a
arisygdc <arisycso@gmail.com>
2021-05-31 23:13:31 +0700

    Create function user_index

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -14,5 +15,10 @@ class AdminController extends Controller
 
     public function index() {
         return view('admin.index');
+    }
+
+    public function users_index() {
+        $data_user = User::select('nip', 'name', 'alamat', 'email')->get();
+        return view('admin.users_index')->with('data_user', $data_user);
     }
 }
