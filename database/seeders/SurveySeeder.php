42771214e128e4258e37629d779eaab018ee8c9e
arisygdc <arisycso@gmail.com>
2021-06-15 14:26:58 +0700

    Create dynamic seed

--- database/seeders/SurveySeeder.php
+++ database/seeders/SurveySeeder.php
@@ -15,12 +15,25 @@ class SurveySeeder extends Seeder
     public function run()
     {
         //
-        Survey::create([
-            'user_id' => 2,
-            'kecamatan' => 'ajung',
-            'pecahan' => 100000,
-            'qlt' => 3,
-            // 'foto'=> bcrypt('image385934gegse')
-        ]);
+        $kecamatan = [
+            'Ambulu', 'Ajung', 'Arjasa', 'Balung', 'Bangsalsari', 'Gumuk Mas', 'Jelbuk', 'Jenggawah', 'Jombang', 'Kalisat', 'Kaliwates', 'Kencong', 'Ledokombo',
+            'Mayang', 'Mumbulsari', 'Pakusari', 'Panti', 'Patrang', 'Puger', 'Rambipuji', 'Semboro', 'Silo', 'Sukorambi', 'Sukowono', 'Sumber Baru',
+            'Sumberjambe', 'Sumbersari', 'Tanggul', 'Tempurejo', 'Umbulsari', 'Wuluhan'
+        ];
+        $user_id = [
+            1,2
+        ];
+        $pecahan = [
+            10000, 20000, 50000, 100000
+        ];
+        for ($i = 1; $i <= 300; $i++) {
+            Survey::create([
+                'user_id' => $user_id[rand(0,(sizeof($user_id)-1))],
+                'kecamatan' => $kecamatan[rand(0,(sizeof($kecamatan)-1))],
+                'pecahan' => $pecahan[rand(0, (sizeof($pecahan)-1))],
+                'qlt' => rand(1,4),
+                // 'foto'=> bcrypt('image385934gegse')
+            ]);
+        }
     }
 }
