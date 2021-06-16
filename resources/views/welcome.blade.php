86e5ed62141bff3e7c77028c08cba0874ccddc21
arisygdc <arisycso@gmail.com>
2021-06-17 00:26:50 +0700

    change map posision

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -101,7 +101,7 @@
     </div>
   </div>
   <script>
-  var map = L.map('map').setView([-8.184486, 113.668076], 9);
+  var map = L.map('map').setView([-8.264486, 113.668076], 10);
 
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
