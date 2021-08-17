b4ffc48018f4af06962956f53244ddcfd8dc58c5
arisygdc <arisycso@gmail.com>
2021-08-17 22:27:01 +0700

    remove survey seeder, error cause need to add foto data to SurveySeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // // \App\Models\User::factory(10)->create();
         $this->call(RoleSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(KecamatanSeeder::class);
         $this->call(PecahanSeeder::class);
-        $this->call(SurveySeeder::class);
+        // $this->call(SurveySeeder::class);
     }
 }
