ef30f3677794d0fb37b99bb0bd79ded0b14d4844
arisygdc <arisycso@gmail.com>
2021-08-20 06:49:20 +0700

    add money value

--- database/seeders/PecahanSeeder.php
+++ database/seeders/PecahanSeeder.php
@@ -16,7 +16,7 @@ class PecahanSeeder extends Seeder
     {
         //
         $pecahan = [
-            1000, 2000, 5000, 10000
+            1000, 2000, 5000, 10000, 20000, 50000, 100000
         ];
 
         foreach($pecahan as $value) {
